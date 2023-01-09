c5268f631806d60b1437f284dd38dfe7f89bacf2
Davysparkz <dabakura@gmail.com>
2023-01-09 15:02:16 +0100

    Preparing the essential UI pieces

---
+++ php-my-editor/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using php_my_editor.Utils;
+
+namespace php_my_editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<int> _lineNumbers;
+        private Dictionary<int, string> _editorContent;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.pme.ToImageSource();
+            this._lineNumbers = new List<int>();
+            this._editorContent = new Dictionary<int, string>();
+        }
+
+        public void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        public void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("NEW COMMAND");
+        }
+
+        private void Editor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var row = editor.GetLineIndexFromCharacterIndex(editor.CaretIndex);
+            var col = editor.CaretIndex - editor.GetCharacterIndexFromLineIndex(row);
+
+            var lineText = editor.GetLineText(row);
+            var lineNumber = row + 1;
+            var charNumber = col;
+
+            cursorPosLabel.Text = $"Line {lineNumber} , Char {charNumber}";
+
+            if (!_lineNumbers.Contains(lineNumber))
+            {
+                _lineNumbers.Add(lineNumber);
+                _editorContent.Add(lineNumber, lineText);
+            }
+            lineNumbers.Text = _lineNumbers.ToNewLinesString();
+        }
+
+        private void Editor_KeyDown(object sender, KeyEventArgs e)
+        {
+            var row = editor.GetLineIndexFromCharacterIndex(editor.CaretIndex);
+            var lineText = editor.GetLineText(row);
+
+            if (e.Key == Key.Back)
+            {
+                if (lineText.Length == 0)
+                {
+                    _lineNumbers.RemoveAt(_lineNumbers.Count);
+                    lineNumbers.Text = _lineNumbers.ToNewLinesString();
+                }
+            }
+        }
+    }
+}
---
+++ php-my-editor/Utils/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace php_my_editor.Utils
+{
+    public static class IconUtil
+    {
+        public static ImageSource ToImageSource(this Icon icon)
+        {
+            var imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions()
+                );
+
+            return imageSource;
+        }
+    }
+
+    public static class Utils
+    {
+        public static string ToNewLinesString(this List<int> list)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
